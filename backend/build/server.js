0103ae5189b2ec9f3c502fb4028ee3cc662e5ecb
petr <iampetermitsel@gmail.com>
2020-07-10 15:37:56 -0400

    Change to TypeScript

--- backend/build/server.js
+++ backend/build/server.js
@@ -1,14 +1,12 @@
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // lib/app.ts
-import express = require('express');
-
+var express = require("express");
 // Create a new express application instance
-const app: express.Application = express();
-
+var app = express();
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
